c13c7b1cc1bcae467eb04cd2cb3b7dfc963456cd
Alex-Harron <aharron.harron@gmail.com>
2021-05-25 12:08:38 -0400

    Done.

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ document.addEventListener("keydown", function(e) {
     if (e.key == "ArrowLeft") {
         moveDodgerLeft();
     }
+});
+
+document.addEventListener("keydown", function(e) {
     if (e.key = "ArrowRight") {
         moveDodgerRight();
     }
